52bd4fe74274dd81a4f2e4e74619a41ddb62b0e2
kangyuxin <1549069709@qq.com>
2021-06-12 21:45:56 +0800

    个人中心页

--- pages/personal/index.js
+++ pages/personal/index.js
@@ -1,11 +1,16 @@
 // pages/personal/index.js
+let startY = 0 // 手指起始的坐标
+let moveY = 0 // 手指移动的坐标
+let moveDistance = 0 // 手指移动的距离
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    coverTransform: 'translateY(0)',
+    coverTransition: ''
   },
 
   /**
@@ -13,6 +18,35 @@ Page({
    */
   onLoad: function (options) {
 
+  },
+
+  handleTouchStart (event) {
+    this.setData({
+      coverTransition: ''
+    })
+    startY = event.touches[0].clientY
+  },
+
+  handleTouchMove (event) {
+    moveY = event.touches[0].clientY
+    moveDistance = moveY - startY
+
+    if (moveDistance <= 0) {
+      return 
+    }
+    if (moveDistance >= 80) {
+      moveDistance = 80
+    }
+    this.setData({
+      coverTransform: `translateY(${moveDistance}rpx)`
+    })
+  },
+
+  handleTouchEnd () {
+    this.setData({
+      coverTransform: `translateY(0rpx)`,
+      coverTransition: 'transform 1s linear'
+    })
   },
 
   /**
